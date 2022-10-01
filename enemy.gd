45d724705bfa273f712b55a68a98639a93b99b71
johnfn <johnfn@gmail.com>
2022-09-30 20:47:08 -0700

    enemy stuff

---
+++ enemy.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+var next_destination = Vector2.ZERO
+
+func _ready():
+  next_destination = position
+
+func choose_next_destination():
+  var deltas = [
+    200 * Vector2.LEFT,
+    200 * Vector2.RIGHT,
+    200 * Vector2.UP,
+    200 * Vector2.DOWN
+  ]
+
+  var new_destination = position + deltas[randi() % deltas.size()]
+
+  next_destination = new_destination
+
+  # TODO: Use this to determine if the new destination is valid
+  # if get_node("/root/World").get_cellv(new_destination) == 0:
+  #   next_destination = new_destination
+
+func _process(_delta):
+  if next_destination.distance_to(position) < 15:
+    choose_next_destination()
+  
+  var direction = position.direction_to(next_destination).normalized()
+
+  velocity = direction * 100
+
+  move_and_slide()
--- player.gd
+++ player.gd
@@ -6,8 +6,6 @@ func _ready():
   pass
 
 func _process(_delta):
-  # Move character when the user presses the arrow keys.
-
   var direction = Input.get_vector(
     "ui_left", 
     "ui_right",
