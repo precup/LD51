4a9ca7e40b786ea149c5da6764b1f3af9183f880
johnfn <johnfn@gmail.com>
2022-09-30 23:59:12 -0700

    make healthbar fade

--- health_bar.gd
+++ health_bar.gd
@@ -1,16 +1,35 @@
 extends Node2D
 
+var current_health = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-  pass # Replace with function body.
+  modulate = Color.TRANSPARENT
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-  pass
-
+# expected that the parent calls this once per tick
 func update(health, max_health):
   var graphic = $graphic
 
   graphic.scale = Vector2(float(health) / float(max_health), 0.3)
+
+  if current_health != health and current_health != 0:
+    current_health = health
+    self.modulate = Color.WHITE
+    eventually_fade_out()
+  
+  current_health = health
+
+
+func eventually_fade_out():
+  var health_when_changed = current_health
+
+  for x in range(150):
+    if health_when_changed != current_health:
+      return
+
+    await get_tree().process_frame
+  
+  # fade out after 1 second
+  var tween = create_tween()
+  
+  if health_when_changed == current_health:
+    tween.tween_property(self, "modulate", Color.TRANSPARENT, 0.4)
