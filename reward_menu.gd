7ef5999bfcb73c2520071f2f6561bfb11a8d19da
Mike Precup <arch@minipepper.games>
2022-10-02 00:04:28 -0700

    Working upgrades

--- reward_menu.gd
+++ reward_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const GUN_SCENE: PackedScene = preload("res://gun.tscn")
+
 @onready var HEADER: Label = $center/panel/vsplit/margin/vbox/header
 @onready var OPTION_HEADER: Label = $center/panel/vsplit/margin2/vsplit/option_header
 @onready var NEW_WEAPON = $center/panel/vsplit/margin/vbox/center/margin/new_weapon
@@ -7,10 +9,12 @@ extends Control
 @onready var UPGRADE_NAME = $center/panel/vsplit/margin/vbox/center/margin/hsplit/vsplit/name
 @onready var UPGRADE_DESCRIPTION = $center/panel/vsplit/margin/vbox/center/margin/hsplit/vsplit/description
 
+@onready var WEAPON1 = $center/panel/vsplit/margin2/vsplit/hbox/margin/weapon1
+@onready var WEAPON2 = $center/panel/vsplit/margin2/vsplit/hbox/margin2/weapon2
+
 var _upgrade_mode: bool = true
 var _upgrade: Modifiers.Gun = Modifiers.Gun.NONE
-var _gun_slots: int = 1
-var _gun_mods: Array = []
+var _gun = null
 
 func _ready() -> void:
   visible = false
@@ -20,12 +24,14 @@ func get_random_upgrade(min_rarity: QuestGlobals.Rarity):
   return Modifiers.Gun.RICOCHET
   
 
-func get_random_max_slots(min_rarity: QuestGlobals.Rarity) -> int:
-  return 2
-  
-
-func get_random_filled_slots(min_rarity: QuestGlobals.Rarity, max_slots: int) -> int:
-  return 1
+func get_random_gun(min_rarity: QuestGlobals.Rarity):
+  var gun = GUN_SCENE.instantiate()
+  var max_mods: int = max((randi() % (min_rarity * 2 + 2)), min_rarity + 2)
+  gun.MAX_MODIFIERS = max_mods
+  gun.UPGRADES = []
+  for i in range(max(1, int(ceil(gun.MAX_MODIFIERS / 2.0)))):
+    gun.UPGRADES.append(get_random_upgrade(min_rarity))
+  return gun
 
 
 func show_reward(reward_type: QuestGlobals.RewardType, min_rarity: QuestGlobals.Rarity) -> void:
@@ -35,6 +41,10 @@ func show_reward(reward_type: QuestGlobals.RewardType, min_rarity: QuestGlobals.
   NEW_WEAPON.visible = reward_type == QuestGlobals.RewardType.REWARD_GUN
   NEW_UPGRADE.visible = reward_type == QuestGlobals.RewardType.REWARD_MOD
   
+  var player_guns: Array = get_tree().get_first_node_in_group("player").get_node("guns").get_children()
+  WEAPON1.display_weapon(player_guns[0], reward_type == QuestGlobals.RewardType.REWARD_MOD)
+  WEAPON2.display_weapon(player_guns[1], reward_type == QuestGlobals.RewardType.REWARD_MOD)
+  
   match reward_type:
     QuestGlobals.RewardType.REWARD_MOD:
       _upgrade_mode = true
@@ -43,15 +53,15 @@ func show_reward(reward_type: QuestGlobals.RewardType, min_rarity: QuestGlobals.
       UPGRADE_DESCRIPTION.text = Modifiers.DESCRIPTIONS[_upgrade]
       HEADER.text = "New Wand Upgrade!"
       OPTION_HEADER.text = "Pick a wand to upgrade:"
+      _gun = null
       
     QuestGlobals.RewardType.REWARD_GUN:
       _upgrade_mode = false
-      _gun_slots = get_random_max_slots(min_rarity)
-      _gun_mods = []
-      for i in range(get_random_filled_slots(min_rarity, _gun_slots)):
-        _gun_mods.append(get_random_upgrade(min_rarity))
+      _gun = get_random_gun(min_rarity)
+      NEW_WEAPON.display_weapon(_gun, false)
       HEADER.text = "New Wand!"
       OPTION_HEADER.text = "Pick a wand to replace:"
+      _upgrade = Modifiers.Gun.NONE
       
     QuestGlobals.RewardType.REWARD_OTHER:
       close_menu()
@@ -63,15 +73,28 @@ func close_menu() -> void:
 
 
 func _on_weapon_1_clicked():
-  print("weapon 1 chosen")
-  close_menu()
+  apply_to_weapon(0)
 
 
 func _on_weapon_2_clicked():
-  print("weapon 2 chosen")
+  apply_to_weapon(1)
+
+
+func apply_to_weapon(i):
+  var gun_node = get_tree().get_first_node_in_group("player").get_node("guns")
+  var old_gun = gun_node.get_child(i)
+  if _upgrade_mode:
+    old_gun.UPGRADES.append(_upgrade)
+  else:
+    _gun.PROJECTILE_NODE = old_gun.PROJECTILE_NODE
+    old_gun.add_sibling(_gun)
+    _gun.visible = old_gun.visible
+    old_gun.visible = false
+    gun_node.remove_child(old_gun)
   close_menu()
 
 
 func _on_trash_clicked():
-  print("trash chosen")
+  if not _upgrade_mode:
+    _gun.visible = false
   close_menu()
--- weapon_display.gd
+++ weapon_display.gd
@@ -14,7 +14,9 @@ func _ready():
     $split/panel.visible = false
   $highlight.visible = false
 
-func display_weapon(upgrades: Array, max_slots: int, can_max_out: bool):
+func display_weapon(gun, can_max_out: bool):
+  var max_slots: int = gun.MAX_MODIFIERS
+  var upgrades: Array = gun.UPGRADES
   maxed_out = max_slots <= len(upgrades) and can_max_out
   $unselectable.visible = maxed_out
   while SLOT_PARENT.get_child_count() < max_slots:
@@ -24,7 +26,7 @@ func display_weapon(upgrades: Array, max_slots: int, can_max_out: bool):
     SLOT_PARENT.get_child(i).queue_free()
   SLOT_PARENT.get_child(0).visible = max_slots > 0
   
-  for i in range(len(max_slots)):
+  for i in range(max_slots):
     var slot = SLOT_PARENT.get_child(i)
     slot.get_node("hsplit/mod_name").text = Modifiers.NAMES[upgrades[i]] if i < len(upgrades) else ""
     slot.get_node("hsplit/full_icon").visible = i < len(upgrades)
@@ -33,7 +35,7 @@ func display_weapon(upgrades: Array, max_slots: int, can_max_out: bool):
 
 func _input(event):
   if event is InputEventMouseButton:
-    if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and $highlight.visible:
+    if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and CLICKABLE and not maxed_out and $highlight.visible:
       emit_signal("clicked")
 
 
