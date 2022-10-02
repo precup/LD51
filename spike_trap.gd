04f0a263eb00a3accccb59011abaa7284c69c03e
Mike Precup <arch@minipepper.games>
2022-10-02 01:18:40 -0700

    Fix spikes to be independent of display FPS and work with pause

--- spike_trap.gd
+++ spike_trap.gd
@@ -2,11 +2,22 @@ extends Area2D
 
 var is_spiky = false
 var bodies_in_trap = []
-@export var off_cycle = false
+@export var STARTING_TIME: float = 0.0
+@export var UP_TIME: float = 2.0
+@export var DOWN_TIME: float = 6.0
+var state_ttl: float = 0
 
 # countdown looks pretty ugly and seems unnecessary imo!
 
 func _ready():
+  var start_time: float = fmod(STARTING_TIME, UP_TIME + DOWN_TIME)
+  if start_time > DOWN_TIME:
+    state_ttl = UP_TIME + DOWN_TIME - start_time
+    is_spiky = true
+  else:
+    state_ttl = DOWN_TIME - start_time
+    is_spiky = false
+  
   $base_enemy._max_health = 4
   $base_enemy._health = 4
   $base_enemy._item_drop_type = "trash"
@@ -17,44 +28,35 @@ func _ready():
   
   var label: Label = $countdown
   label.text = "" 
-  run_state_machine()
+  
 
-func run_state_machine():
-  while true:
-    is_spiky = false
+func _physics_process(delta):
+  if state_ttl - delta < 0:
+    is_spiky = not is_spiky
+    if is_spiky:
+      state_ttl = UP_TIME
+    else:
+      state_ttl = DOWN_TIME
     update_graphics()
-    
-    for x in range(3):
-      # $countdown.text = "%d" % x
-      
-      if off_cycle:
-        off_cycle = false
-        continue
-      
-      for tick in range(120):
-        await get_tree().process_frame
-    
-    is_spiky = false
-    update_graphics()
-    
-    $graphic_triggered.visible = true
-    $graphic.visible = false
-    
-    for tick in range(120):
-      await get_tree().process_frame
-      
-      for body in bodies_in_trap:
+  else:
+    state_ttl -= delta
+  
+  if is_spiky:
+    for body in bodies_in_trap:
         # TODO: Don't damage more than once per cycle
         if body.has_method("damage"):
           body.damage(1)
+
 
 func update_graphics():
   $graphic_triggered.visible = is_spiky
   $graphic.visible = not is_spiky
   # $countdown.visible = not is_spiky
 
+
 func _on_spike_trap_body_entered(body):
   bodies_in_trap.append(body)
+
 
 func _on_spike_trap_body_exited(body):
   bodies_in_trap.erase(body)
