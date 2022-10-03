5259b0f1c0087c330b285cba84f110f4c65bd04b
johnfn <johnfn@gmail.com>
2022-10-02 20:50:44 -0700

    add dialog triggers as well

--- npc.gd
+++ npc.gd
@@ -1,33 +1,23 @@
 extends Area2D
 
 @export var dialog_text = "Hello."
-var player_in_area = false
+var triggered = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-  $position/click_to_talk_label.visible = false
-  $animation.play("pulse")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-  $position/click_to_talk_label.visible = player_in_area
+func _on_npc_body_entered(body):
+  if triggered:
+    return
   
-  if player_in_area:
-    if Input.is_action_just_pressed("shoot") and not $"/root/root/ui/dialog".visible:
+  var player = $"/root/root/references".get_player()
+
+  if player == body:
+    if not $"/root/root/ui/dialog".visible:
+      triggered = true
+      
       $"/root/root/ui/dialog/label".text = dialog_text
       
       await get_tree().process_frame
       get_tree().paused = true
       $"/root/root/ui/dialog".visible = true
 
-func _on_npc_body_entered(body):
-  var player = $"/root/root/references".get_player()
-
-  if player == body:
-    player_in_area = true
-
 func _on_npc_body_exited(body):
-  var player = $"/root/root/references".get_player()
-
-  if player == body:
-    player_in_area = false
+  pass
