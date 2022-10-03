a278746cb3c071dd385b7f66d56046bcf3dafdaf
Mike Precup <arch@minipepper.games>
2022-10-03 02:27:32 -0700

    fixes

--- barrel.gd
+++ barrel.gd
@@ -6,8 +6,9 @@ func _ready():
   $base_enemy._item_drop_type = "trash"  
   $base_enemy._stats_on_kill.append(QuestGlobals.StatTrack.STAT_BREAK_OBJECT)
 
+
 var threshold = 0
-func _process(delta):
+func _physics_process(delta):
   if $base_enemy._health < 4 and threshold == 0:
     threshold = 1
     $graphic.texture = load("res://assets/pot_hit.png")
---
+++ sfx.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+func _ready():
+  pass
+#  for sound in get_children():
+#    sound.loop = false
