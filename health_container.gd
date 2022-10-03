1205b32a7cfe4bfdd11963f2ecfe170823876cad
johnfn <johnfn@gmail.com>
2022-10-02 21:59:52 -0700

    Pulse hearts

--- health_container.gd
+++ health_container.gd
@@ -3,6 +3,7 @@ extends HBoxContainer
 var player: Player
 var cur_health = 0
 var cur_max_health = 0
+var active_heart: Node2D = null
 
 func _ready():
   player = get_tree().get_first_node_in_group("player")
@@ -18,22 +19,28 @@ func _process(delta):
     
     update_hearts()
 
-
 func update_hearts():
   var health = player.health()
   var max_health = player.max_health()
   var hearts = get_children()
   
   var health_left = health
+  var next_active_heart: Node2D = null
   
   for i in range(hearts.size()):
     var heart = hearts[i]
 
     for child in heart.get_children():
+      if child.name == "animation_player":
+        continue
+        
       var heart_sprite: TextureRect = child
 
       child.visible = false
-      
+    
+    if health_left <= 2:
+      next_active_heart = heart
+    
     if health_left >= 2:
       heart.get_node("heart_full").visible = true
     elif health_left >= 1:
@@ -42,3 +49,9 @@ func update_hearts():
       heart.get_node("heart_empty").visible = true
       
     health_left -= 2
+  
+  if next_active_heart != active_heart:
+    if active_heart != null:
+      active_heart.get_node("animation_player").seek(0, true)
+    if next_active_heart != null:
+      next_active_heart.get_node("animation_player").play("pulse")
